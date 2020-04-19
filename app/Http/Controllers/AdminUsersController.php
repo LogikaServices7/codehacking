8e7c574b5d4e117c340a9cdab9c6a56030c3b061
McLmnop <mclmnop305@gmail.com>
2020-04-19 19:06:13 +0800

    all is well B4 our 1st post

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -9,7 +9,9 @@ use App\Role;
 use App\User;
 
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 
 
@@ -40,6 +42,8 @@ class AdminUsersController extends Controller
 
 
         $roles = Role::pluck('name','id')->all();
+
+
 
         return view('admin.users.create', compact('roles'));
     }
@@ -84,6 +88,7 @@ class AdminUsersController extends Controller
        
         User::create($input);
 
+        Session::flash('created_user','The user has been created successfully');
 
         
        return redirect('/admin/users');
@@ -171,5 +176,15 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+
+        $user = User::findOrFail($id);
+
+        unlink(public_path() . $user->photo->file);
+
+        $user->delete();
+
+        Session::flash('deleted_user','The user has been deleted');
+
+        return redirect('/admin/users');
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+
+
+
+
+@section('content')
+
+
+<h1 class="text-center bg-danger">Oops! 404! Wrong page Weenie Head</h1>
+
+
+@stop
